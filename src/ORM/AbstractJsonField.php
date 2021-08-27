fd7ea5dba1e0d518dfda6aa03c3b03c53a3f0a0d
Kate <hmk12011506@gmail.com>
2021-08-27 17:18:22 +0300

    Сериалайзер - fix

--- src/ORM/AbstractJsonField.php
+++ src/ORM/AbstractJsonField.php
@@ -1,7 +1,9 @@
 <?php
 declare(strict_types=1);
 
-namespace ArtSkills\Lib\Serializer;
+namespace ArtSkills\ORM;
+
+use ArtSkills\Lib\Serializer\SerializerFactory;
 
 abstract class AbstractJsonField implements ArraySerializerInterface
 {
