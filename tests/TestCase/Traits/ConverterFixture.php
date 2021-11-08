dbe0db17a8feb5d568ad3d7ea36bde06860816da
Kate <hmk12011506@gmail.com>
2021-11-08 10:21:22 +0300

    [WB-546] fix

--- tests/TestCase/Traits/ConverterFixture.php
+++ tests/TestCase/Traits/ConverterFixture.php
@@ -9,9 +9,12 @@ class ConverterFixture
 {
     use Converter;
 
+    /** @var int hghjgj */
     public int $intField;
 
+    /** @var string hghjgj */
     public string $stringField;
 
+    /** @var bool hghjgj */
     public bool $boolField;
 }
