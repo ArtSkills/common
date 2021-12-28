db8228a65e00454433ec9f9dccbeb198d85af280
aleks <alekse-assd@rambler.ru>
2021-12-28 18:23:18 +0300

    [WB-756] Перенес методы

---
+++ src/TestSuite/PermanentMocks/PermanentMocksCollection.php
@@ -0,0 +1,93 @@
+<?php
+declare(strict_types=1);
+
+namespace ArtSkills\TestSuite\PermanentMocks;
+
+use ArtSkills\Error\InternalException;
+use ArtSkills\Filesystem\Folder;
+use ArtSkills\Lib\Env;
+use ArtSkills\Lib\Misc;
+use ArtSkills\TestSuite\ClassMockEntity;
+
+/**
+ * Класс для подмены методов, необходимых только в тестовом окружении
+ */
+class PermanentMocksCollection
+{
+    /**
+     * Набор постоянных моков
+     *
+     * @var ClassMockEntity[]
+     */
+    private static array $_permanentMocksList = [];
+
+    /**
+     * Отключённые постоянные моки
+     *
+     * @var array<string, bool> className => true
+     */
+    private static array $_disabledMocks = [];
+
+    /**
+     * Инициалилзируем подмену методов
+     *
+     * @return void
+     * @throws InternalException
+     */
+    public static function init(): void
+    {
+        $permanentMocks = [
+            // folder => namespace
+            __DIR__ . '/PermanentMocks' => Misc::namespaceSplit(MockFileLog::class)[0],
+        ];
+
+        $projectMockFolder = Env::getMockFolder();
+        if (!empty($projectMockFolder)) {
+            if (Env::hasMockNamespace()) {
+                $projectMockNs = Env::getMockNamespace();
+            } else {
+                throw new InternalException('Не задан неймспейс для классов-моков');
+            }
+            $permanentMocks[$projectMockFolder] = $projectMockNs;
+        }
+
+        foreach ($permanentMocks as $folder => $mockNamespace) {
+            $dir = new Folder($folder);
+            $files = $dir->find('.*\.php');
+            foreach ($files as $mockFile) {
+                $mockClass = $mockNamespace . '\\' . str_replace('.php', '', $mockFile);
+                if (empty(self::$_disabledMocks[$mockClass])) {
+                    /** @var ClassMockEntity $mockClass */
+                    $mockClass::init();
+                    self::$_permanentMocksList[] = $mockClass;
+                }
+            }
+        }
+    }
+
+    /**
+     * Уничтожаем моки
+     *
+     * @return void
+     */
+    public static function destroy(): void
+    {
+        foreach (self::$_permanentMocksList as $mockClass) {
+            $mockClass::destroy();
+        }
+
+        self::$_permanentMocksList = [];
+        self::$_disabledMocks = [];
+    }
+
+    /**
+     * Отключение постоянного мока
+     *
+     * @param string $mockClass Путь к классу который мокаем
+     * @return void
+     */
+    public static function disableMock(string $mockClass): void
+    {
+        self::$_disabledMocks[$mockClass] = true;
+    }
+}
