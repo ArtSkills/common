a2d3a2ba52393fa8bf4eae4b740762c54acb2582
Igor Smirnov <i.smirnov@texrus.com>
2022-02-10 14:37:38 +1000

    добавил action unit test

--- test-app-conf/app_local_ci.php
+++ test-app-conf/app_local_ci.php
@@ -1,2 +1,36 @@
 <?php
-return [];
+
+use Cake\Database\Connection;
+use Cake\Database\Driver\Mysql;
+
+return [
+    'Datasources' => [
+        'default' => [
+            'className' => Connection::class,
+            'driver' => Mysql::class,
+            'persistent' => false,
+            'host' => '127.0.0.1',
+            'username' => 'root',
+            'password' => 'root',
+            'database' => 'cakephp',
+            'encoding' => 'utf8',
+            'timezone' => 'SYSTEM',
+            'cacheMetadata' => true,
+            'quoteIdentifiers' => true,
+        ],
+        'test' => [
+            'className' => Connection::class,
+            'driver' => Mysql::class,
+            'persistent' => false,
+            'host' => '127.0.0.1',
+            'username' => 'root',
+            'password' => 'root',
+            'database' => 'cakephp_test',
+            'encoding' => 'utf8',
+            'timezone' => 'SYSTEM',
+            'cacheMetadata' => true,
+            'quoteIdentifiers' => true,
+            'init' => ['SET FOREIGN_KEY_CHECKS=0'],
+        ],
+    ],
+];
