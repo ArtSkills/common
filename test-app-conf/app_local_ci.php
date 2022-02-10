7d08e16caebbd1c118ce8241a5c65d54d4f6edff
Igor Smirnov <i.smirnov@texrus.com>
2022-02-10 15:37:57 +1000

    добавил action unit test

--- test-app-conf/app_local_ci.php
+++ test-app-conf/app_local_ci.php
@@ -33,10 +33,4 @@ return [
             'init' => ['SET FOREIGN_KEY_CHECKS=0'],
         ],
     ],
-    
-  'EmailTransport' => ['test' => ['className' => 'ArtSkills.TestEmail']],
-  'debug' => true,
-  'Security' => ['salt' => '7c47f1e793a39c7f518efc6b909b920ed5ba7a7470efc0501f2960973b7954dd'],
-  'Sentry' => ['dsn' => ''],
-  'testServerName' => 'eggheads.solutions',
 ];
