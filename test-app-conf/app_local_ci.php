1acaae4fea304e516550213ed4f4527c1b87fb23
Igor Smirnov <i.smirnov@texrus.com>
2022-02-10 14:56:47 +1000

    добавил action unit test

--- test-app-conf/app_local_ci.php
+++ test-app-conf/app_local_ci.php
@@ -34,5 +34,9 @@ return [
         ],
     ],
     
-    'Sentry' => ['dsn' => ''],
+  'EmailTransport' => ['test' => ['className' => 'ArtSkills.TestEmail']],
+  'debug' => true,
+  'Security' => ['salt' => '7c47f1e793a39c7f518efc6b909b920ed5ba7a7470efc0501f2960973b7954dd'],
+  'Sentry' => ['dsn' => ''],
+  'testServerName' => 'eggheads.solutions',
 ];
