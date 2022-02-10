37e91c2fd743e87850e2de604f04be5085b72175
Igor Smirnov <i.smirnov@texrus.com>
2022-02-10 14:25:09 +1000

    добавил action unit test

--- test-app-conf/app_local_ci.php
+++ test-app-conf/app_local_ci.php
@@ -1,7 +1,2 @@
 <?php
-return [
-    'Datasources' => ['test' => ['username' => 'root', 'password' => 'root', 'host' => '127.0.0.1', 'port' => '3306', 'database' => 'cakephp_test', 'persistent' => false, 'encoding' => 'utf8'], 'default' => ['username' => 'root', 'password' => 'root', 'host' => '127.0.0.1', 'port' => '3306', 'database' => 'cakephp', 'persistent' => false, 'encoding' => 'utf8']],
-    'EmailTransport' => ['test' => ['className' => 'ArtSkills.TestEmail']],
-    'debug' => true,
-    'testServerName' => 'eggheads.solutions',
-    ];
+return [];
